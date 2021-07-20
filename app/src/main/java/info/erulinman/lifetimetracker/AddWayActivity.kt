4404e0864504174f21f4d2b7f65df056a517461f
erulinman <erwinstein.contact@gmail.com>
2021-07-20 14:23:17 +0300

    Update

--- app/src/main/java/info/erulinman/lifetimetracker/AddWayActivity.kt
+++ app/src/main/java/info/erulinman/lifetimetracker/AddWayActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 
 import androidx.appcompat.app.AppCompatActivity
-import info.erulinman.lifetimetracker.R
 
 import info.erulinman.lifetimetracker.databinding.ActivityAddWayBinding
 
