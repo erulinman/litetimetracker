45a7e7075f7429aff048f099ab90ebafd826d890
Erulinman <erulinman.work@protonmail.com>
2021-10-12 07:51:00 +0300

    Remove unused AddPresetAdapter

--- app/src/main/java/info/erulinman/lifetimetracker/selection/CategoryItemDetailsLookup.kt
+++ app/src/main/java/info/erulinman/lifetimetracker/selection/CategoryItemDetailsLookup.kt
@@ -1,7 +1,6 @@
 package info.erulinman.lifetimetracker.selection
 
 import android.view.MotionEvent
-
 import androidx.recyclerview.selection.ItemDetailsLookup
 import androidx.recyclerview.widget.RecyclerView
 import info.erulinman.lifetimetracker.adapters.CategoryAdapter
@@ -9,8 +8,8 @@ import info.erulinman.lifetimetracker.adapters.CategoryAdapter
 class CategoryItemDetailsLookup(private val recyclerView: RecyclerView) : ItemDetailsLookup<Long>() {
     override fun getItemDetails(event: MotionEvent): ItemDetails<Long>? {
         val view = recyclerView.findChildViewUnder(event.x, event.y)
-        if (view != null) {
-            return (recyclerView.getChildViewHolder(view) as CategoryAdapter.CategoryViewHolder)
+        view?.let {
+            return (recyclerView.getChildViewHolder(it) as CategoryAdapter.CategoryViewHolder)
                 .getItemDetails()
         }
         return null
